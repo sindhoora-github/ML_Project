bf30e6fa0c68918749d52011f35b1b8b334360a2
sindhoora-github <119159247+sindhoora-github@users.noreply.github.com>
2025-12-18 17:33:00 +0530

    Update app.py

--- EFP_project/deployment/app.py
+++ EFP_project/deployment/app.py
@@ -16,12 +16,12 @@ Please enter the sensor and configuration data below to get a prediction.
 """)
 
 # User input
-engine_rpm = st.number_input("Engine rpm", min_value=61.0000, max_value=2239.0000, value=298.0, step=0.1)
-lub_oil_pres = st.number_input("Lub oil pressure", min_value=0.003384, max_value=7.2655, value=324.0, step=0.1)
-fuel_pres= st.number_input("Fuel pressure", min_value=0.0031, max_value=21.1383, value=1400)
-coolant_pres = st.number_input("Coolant pressure", min_value=0.0024, max_value=7.4785, value=40.0, step=0.1)
-lub_oil_temp = st.number_input("lub oil temp", min_value=71.3219, max_value=89.5807, value=10)
-coolant_temp = st.number_input("Coolant temp", min_value=61.6733, max_value=195.5279, value=10)
+engine_rpm = st.number_input("Engine rpm", min_value=61.0000, max_value=2239.0000, value=876.0, step=0.1)
+lub_oil_pres = st.number_input("Lub oil pressure", min_value=0.003384, max_value=7.2655, value=2.9416, step=0.1)
+fuel_pres= st.number_input("Fuel pressure", min_value=0.0031, max_value=21.1383, value=16.1938)
+coolant_pres = st.number_input("Coolant pressure", min_value=0.0024, max_value=7.4785, value=2.4645, step=0.1)
+lub_oil_temp = st.number_input("lub oil temp", min_value=71.3219, max_value=77.6409, value=10)
+coolant_temp = st.number_input("Coolant temp", min_value=61.6733, max_value=82.4457, value=10)
 
 # Assemble input into DataFrame
 input_data = pd.DataFrame([{
