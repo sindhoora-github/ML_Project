7d13b0d92176b3403e4a81e93c0d362126dcb743
sindhoora-github <119159247+sindhoora-github@users.noreply.github.com>
2025-12-18 14:11:41 +0530

    Update hosting.py

--- EFP_project/hosting/hosting.py
+++ EFP_project/hosting/hosting.py
@@ -3,7 +3,7 @@ import os
 
 api = HfApi(token=os.getenv("HF_TOKEN"))
 api.upload_folder(
-    folder_path="/content/EFP_project/deployment",     # the local folder containing your files
+    folder_path="EFP_project/deployment",     # the local folder containing your files
     repo_id="sindhoorasuresh/ML-Project",          # the target repo
     repo_type="space",                      # dataset, model, or space
     path_in_repo="",                          # optional: subfolder path inside the repo
